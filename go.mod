763d81577f93053bd96847bd5c02feeb4f8782b4
David Schoen <dave@lyte.id.au>
2022-10-08 21:57:46 +1100

    Serial connection works

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/neerolyte/select-unplugged
 
 go 1.19
+
+require (
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07 // indirect
+	golang.org/x/sys v0.0.0-20221006211917-84dc82d7e875 // indirect
+)
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	"github.com/tarm/serial"
+)
+
+func main() {
+	c := &serial.Config{Name: "/dev/ttyUSB0", Baud: 57600}
+	s, err := serial.OpenPort(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	n, err := s.Write([]byte("Q\x00\x00\xa0\x00\x00\x9d\x4ab"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	buf := make([]byte, 128)
+	n, err = s.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%q", buf[:n])
+}
