970a5168b1480e73998e1c7ad2c948bde0f029b0
David Schoen <dave@lyte.id.au>
2022-10-10 22:09:41 +1100

    Mock one send with mockery

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
 	github.com/stretchr/testify v1.8.0 // indirect
 	golang.org/x/sys v0.0.0-20221006211917-84dc82d7e875 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- sp/protocol_test.go
+++ sp/protocol_test.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 	"testing"
 
+	"github.com/neerolyte/select-unplugged/sp/mocks"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -27,6 +28,23 @@ func (c *ConnectionMock) Write(data []byte) {
 
 func (c *ConnectionMock) Close() {
 	strCalls = append(strCalls, "Close()")
+}
+
+func TestProtocolSend(t *testing.T) {
+	connection := mocks.NewConnection(t)
+
+	req := NewRequestQuery(Area{
+		address: 0x01,
+		words:   1,
+	})
+	connection.On("Write", []byte(req)).Return()
+	connection.On("Read", 14).Return([]byte("some-response"))
+
+	protocol := NewProtocol(connection)
+	res, err := protocol.Send(req)
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, []byte("some-response"), []byte(res))
 }
 
 func TestProtocolLocking(t *testing.T) {
